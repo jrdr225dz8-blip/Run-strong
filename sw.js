d2f9540314602d8725a34a080d0f2034c2f7f67d
jrdr225dz8-blip <jrdr225dz8@privaterelay.appleid.com>
2026-08-15 21:08:44 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="run-strong-recovery-v8";
-const ASSETS=["./","./index.html","./styles-v3.css?v=20260815-3","./app-v3.js?v=20260815-3","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
+const CACHE="run-strong-recovery-v8b-unique-js";
+const ASSETS=["./","./index.html","./styles-v3.css?v=20260815-3","./app-v8.js?v=20260815-8b","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{
